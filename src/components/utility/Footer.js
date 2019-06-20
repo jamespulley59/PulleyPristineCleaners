138133165abead36a736a87a7d22855f2417adc1
TSN Student <student@ulem.org>
2019-06-20 10:22:12 -0400

    test

--- src/components/utility/Footer.js
+++ src/components/utility/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import axios from "axios";
+
 
 export default class Footer extends Component {
     render() {
         return (
             <div>
-                <Link className='btn btn block' to='ContactUs/'>Contact Us
+                <Link className='btn btn block' to='/ContactUs'>Contact Us
                 </Link>                
             </div>
         )
