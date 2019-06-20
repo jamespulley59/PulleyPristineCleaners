263f100a44052925b99eb10ee060aa99d317f277
Lillian Chan <lchan51@hotmail.com>
2019-06-20 10:17:23 -0400

    image files

--- src/components/utility/Footer.js
+++ src/components/utility/Footer.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom"
 
 export default class Footer extends Component {
+    
+
     render() {
+        const {uid}= this.props
         return (
-            <div>
-                <Link className='btn btn block' to='ContactUs/'>Contact Us
-                </Link>                
-            </div>
+            <nav className="navbar navbar-dark bg-primary fixed-bottom">
+            <div className="full-width">
+            <Link className="float-right" to="../user/contactUs">
+              </Link>
+              </div>
+          </nav>
         )
     }
-}
--- src/components/utility/Header.js
+++ src/components/utility/Header.js
@@ -1,13 +1,3 @@
-import React, { Component } from 'react'
-
-export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                <header className ='btn btn-block'>
-                    <hi>Pulley's Elite Clean</hi>
-                </header>
-            </div>
-        )
-    }
-}
+<h1>PULLEY'S PRISTINE CLEAN</h1>
+<h2>Commercial | Residential | Cleaning Services</h2>
+<h3>774-441-0026</h3>
