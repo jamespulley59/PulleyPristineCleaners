5608d1c374eb1187f16a16ad6e173055440c1d7e
publaptops <jamespulley59@gmail.com>
2019-06-22 17:29:58 -0400

    homepage added text, need to split into 3

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -24,13 +24,30 @@ return (
                         <Link to = {`/BeforeAndAfter${BeforeAndAfter}`}>Before and After Photos</Link>
                         <Link to = {`/GiftCertificates${GiftCertificates}`}>Gift Certificates Available</Link>  
                         <Link to = {`/Comments${Comments}`}>Comments?</Link>
-                        <Link to = {`/${Home}`}>Home Page</Link>
+                        {/* <Link to = {`/${Home}`}>Home Page</Link> */}
                 </nav>
             </div>
 
-            <div className='text-center'>
-            <img className='homeimg' src={pic1} alt="" />
-            </div>
+            <section className='pos'>
+                <div className='text-center'>
+                    <div className='pos1'>
+                        <h3>Residential Services</h3>
+                            <p classname='residential'>Our goal is to make you smile when you get home. Home to your clean safe haven. You have enough on your plate; family, work, leisure activities. Pulley's Pristine Clean allows you to live your life knowing you're coming home. Not coming home to another job (cleaning), but coming to relax in comfort and contemplate what's important; your real life. Allow us to take care of the small, yet time consuming, detail of keeping your home showcase clean. (mission statemnet in italics?) Click on Contact Us at the bottom of this page today and begin enjoying a more relaxed lifestyle.  
+                            </p>
+                    </div>
+
+                    <div className='pos2'>
+                        <img className='homeimg' src={pic1} alt="" />
+                    </div>
+
+                    <div className='pos3'>
+                        <h3>Commercial Services</h3>
+                            <p>We'll clean the shit out of your office                    
+                            </p>
+                    </div>         
+                </div>
+            </section> 
+           
 
         <Footer />
          
