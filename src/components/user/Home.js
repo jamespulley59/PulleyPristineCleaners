9c96349d809801767adee5c0c20977d62561c51e
jamespulley59 <jamespulley59@gmail.com>
2019-06-24 09:28:08 -0400

    sdf

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -5,9 +5,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <header>shaunagit add .
-                    git commit -m 'test'
-                </header> 
+               
             </div>
         )
     }
