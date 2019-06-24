3d8f7ad94a4f926c032bf23e56a72d3071fb0e95
jamespulley59 <jamespulley59@gmail.com>
2019-06-24 09:21:20 -0400

    updating

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -14,63 +14,53 @@ export default class Home extends Component {
 render() {
      
 return (
-    <div>
-        <Header />
-            <div>
-                <nav className="navbar navbar-dark  ">
-                    {/* <h2>Home Page</h2>           */}
-                        <Link to = {`/LoginProfile${LoginProfile}`}>Login</Link>
-                        <Link to = {`/ScheduleServices${ScheduleServices}`}>See or Schedule Service(s)</Link>
-                        <Link to = {`/BeforeAndAfter${BeforeAndAfter}`}>Before and After Photos</Link>
-                        <Link to = {`/GiftCertificates${GiftCertificates}`}>Gift Certificates Available</Link>  
-                        <Link to = {`/Comments${Comments}`}>Comments?</Link>
-                        {/* <Link to = {`/${Home}`}>Home Page</Link> */}
-                </nav>
+<div>
+    <Header />
+        <div>
+            <nav className="navbar navbar-dark  ">
+                {/* <h2>Home Page</h2>           */}
+                    <Link to = {`/LoginProfile${LoginProfile}`}>Login</Link>
+                    <Link to = {`/ScheduleServices${ScheduleServices}`}>See or Schedule Service(s)</Link>
+                    <Link to = {`/BeforeAndAfter${BeforeAndAfter}`}>Before and After Photos</Link>
+                    <Link to = {`/GiftCertificates${GiftCertificates}`}>Gift Certificates Available</Link>  
+                    <Link to = {`/Comments${Comments}`}>Comments?</Link>
+                    {/* <Link to = {`/${Home}`}>Home Page</Link> */}
+            </nav>
+        </div>
+
+    <section>
+        <div className="row">
+        <div className='text-center'>                
+            <div className='col-lg-4 d-none d-lg-block'>
+                <h3>Residential Services</h3>
+                    <p> Our goal is to make you smile when you get home.                               
+                    You have enough on your plate; family, work, leisure activities.
+                    Imagine coming home, knowing you don't have another job (cleaning) to do.
+                    You're coming home to relax in comfort. Allow us to take care of the small yet time consuming detail of keeping your home showcase clean 
+                        
+                    Click on Contact Us at the bottom of this page now and find out how much time Pulley's Pristine Clean can save you.
+                    </p>  
+            </div>           
+            
+                <div className='col-lg-4 d-none d-lg-block'>
+                    <img className='homeimg' src={pic1} alt="" />
+                </div>                      
+            
+                <div className='col-lg-4 d-none d-lg-block'>
+                    <h3>Commercial Services</h3>
+                        <p>We'll clean the shit out of your office                    
+                        </p>
+                </div>
+                     
             </div>
+        </div>
+    </section> 
+    
 
-            <section>
-                <div class="row">
-  {/* <div class="column"></div>
-  <div class="column"></div>
-  <div class="column"></div>
-</div> */}
-
-                <div className='text-center'>
-                     <div className='column'>
-                        <div className='col-lg-4 d-none d-lg-block'>
-                            <h3>Residential Services</h3>
-                                <p> Our goal is to make you smile when you get home.                               
-                                You have enough on your plate; family, work, leisure activities.
-                                Imagine coming home, knowing you don't have another job (cleaning) to do.
-                                You're coming home to relax in comfort. Allow us to take care of the small yet time consuming detail of keeping your home showcase clean 
-                                 
-                                Click on Contact Us at the bottom of this page now and find out how much time Pulley's Pristine Clean can save you.
-                                </p>  
-                        </div>
-                    </div>
-
-                    <div className='column'>
-                        <div className='col-lg-4 d-none d-lg-block'>
-                            <img className='homeimg' src={pic1} alt="" />
-                        </div>
-                    </div>
-                    
-                    <div classnName='column'> 
-                         <div className='col-lg-4 d-none d-lg-block'>
-                             <h3>Commercial Services</h3>
-                                <p>We'll clean the shit out of your office                    
-                                </p>
-                        </div>
-                    </div>         
-                </div>
-                </div>
-            </section> 
-           
-
-        <Footer />
-         
-    </div>                    
+    <Footer />
         
+</div>                    
+    
 )
 }
 }
