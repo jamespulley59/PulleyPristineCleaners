b13eb9ce37bcb2d0a631a5a303b0aad1ede23101
jamespulley59 <jamespulley59@gmail.com>
2019-06-24 14:22:15 -0400

    home

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -15,7 +15,7 @@ export default class Home extends Component {
 render() {
      
 return (
-    <div>   
+<div>   
     <Header />
     <nav className="navbar navbar-dark  ">
         {/* <h2>Home Page</h2>           */}
@@ -28,7 +28,7 @@ return (
     </nav>
     <section>        
         <div className="row">
-            <div className="col-lg-4 text-center">
+            <div className="col-lg-6 text-center">
             <h3>Residential Services</h3>
                 <p>
                     Our goal is to make you smile when you get home.                               
@@ -36,16 +36,18 @@ return (
                     Imagine coming home, knowing you don't have another job (cleaning) to do.
                     You're coming home to relax in comfort. Allow us to take care of the small yet time consuming detail of keeping your home showcase clean Click on Contact Us at the bottom of this page now and find out how much time Pulley's Pristine Clean can save you.
                 </p>
-                <div>
-                <img className='img-fluid homeimg' src={discount} alt="" />
-            </div>  
+                <div className="text-center">
+                <img className='img-fluid' src={discount} alt="" />
+                </div>
             </div>
-            <div className="col-lg-4 text-center">
+             
+            
+            <div className="col-lg-6 text-center">
+                <h3>Commercial Services</h3>
+                <p>Everyone loves a clean office, especially your clients. We can keep your space Pulley's Pristine Clean on a daily, weekly or bi-weekly basis. A clean workspace says something to visitors and staff. Let your office's appearance speak to your clients. Click on Contact Us below to arrange a free in-office estimate.</p>
+            <div className="text-center">
                 <img className='img-fluid homeimg' src={pic2} alt="" />
             </div>
-            <div className="col-lg-4 text-center">
-                <h3>Commercial Services</h3>
-                <p>We'll clean the shit out of your office</p>
             </div>
         </div>
     </section> 
--- src/components/utility/Header.js
+++ src/components/utility/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import css from './src/App.css'
+
 
 export default class Header extends Component {
     render() {
