6546c0b3574c36f20fb20cef0639a9db14b163fd
publaptops <jamespulley59@gmail.com>
2019-06-25 18:40:54 -0400

    tweeking some files

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -17,38 +17,43 @@ export default class Home extends Component {
 render() {
      
 return (
-<div>   
+<div> 
+      {/* Header from utility */}
+      {/* Navbar from utility */}
     <section>
+        {/* tag to keep rows together */}
         <div className="row">
+            {/* what size is this part of the row */}
             <div className="col-lg-6 text-center">
+                {/* wanted this text outside of p to make it bigger */}
             <h3>Residential Services</h3>
+            {/* talking */}
                 <p>
                     Our goal is to make you smile when you get home.                               
                     You have enough on your plate; family, work, leisure activities.
                     Imagine coming home, knowing you don't have another job (cleaning) to do.
-                    You're coming home to relax in comfort. Allow us to take care of the small yet time consuming detail of keeping your home showcase clean Click on Contact Us at the bottom of this page now and find out how much time Pulley's Pristine Clean can save you.
+                    You're coming home to relax in comfort. Allow us to take care of the small yet time consuming detail of keeping your home showcase clean. Click on Contact Us at the bottom of this page now and find out how much time Pulley's Pristine Clean can save you.
                 </p>
+                {/* how i want img aligned */}
                 <div className="text-center">
+                    {/* img from import above from import file */}
                 <img className='img-fluid' src={discount} alt="" />
                 </div>
-            </div>
-             
-            
+            </div>             
+            {/* what size is this part */}
             <div className="col-lg-6 text-center">
+                {/* different size text befor p */}
                 <h3>Commercial Services</h3>
+                {/* talking */}
                 <p>Everyone loves a clean office, especially your clients. We can keep your space Pulley's Pristine Clean on a daily, weekly or bi-weekly basis. A clean workspace says something to visitors and staff. Let your office's appearance speak to your clients. Click on Contact Us below to arrange a free in-office estimate.</p>
             <div className="text-center">
+                {/* where i got pic from (from above import) */}
                 <img className='img-fluid homeimg' src={pic2} alt="" />
             </div>
             </div>
         </div>
-    </section> 
-    
-
-    <Footer />
-        
-</div>                    
-    
-)
-}
+    </section>    
+{/* Footer from utility */}      
+</div>                      
+)}
 }
--- src/components/utility/Footer.js
+++ src/components/utility/Footer.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 export default class Footer extends Component {    
 
 render() {
-    //const {uid}= this.props
+    
     return (
         <div className='footer'>                
-            <Link className='btn btn-block fixed-bottom' to="/contact">
+            <Link className='btn-outline fixed-bottom' to="/contact">
                 <h2 className='text-center' >Contact Us</h2>
             </Link>               
         </div>
