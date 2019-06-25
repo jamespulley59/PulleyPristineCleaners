19a6d2bb1b5fb81add0f0fb40e3db696950e8829
jamespulley59 <jamespulley59@gmail.com>
2019-06-25 15:55:54 -0400

    started login

---
+++ src/components/user/LoginProfile.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import Header from '../utility/Header'
+import Navbar from '../utility/Navbar'
+import Footer from '../utility/Footer'
+import ContactUs from './ContactUs'
+import Home from './Home'
+import ScheduleServices from './ScheduleServices'
+import BeforeAndAfter from './BeforeAndAfter'
+import GiftCertificates from './GiftCertificates'
+import Comments from './Comments'
+import pic2 from "../img/pic2.jpg"
+import discount from "../img/discount.png"
+
+export default class LoginProfile extends Component {
+    render() {
+        return (
+            <div>
+                <h4 className='logintext text-center'>Please create your username and password (Remember, you do not need to login to schedule an in-home estimate. Simply get in touch with us by clicking on Contact Us at the bottom of this page.). If this is your first time logging in, please complete all fields to create your profile in our records.
+                </h4>
+            </div>
+
+
+)}}
+
