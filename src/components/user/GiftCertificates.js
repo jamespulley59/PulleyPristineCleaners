b1e56ae475ae3506e05998599455a574c919ca02
Lillian Chan <lchan51@hotmail.com>
2019-07-03 11:39:00 -0400

    not quite final

--- src/components/user/GiftCertificates.js
+++ src/components/user/GiftCertificates.js
@@ -10,35 +10,44 @@ export default class GiftCertificates extends Component {
   render() {
     return (
       <div>
-        <h4 className="busy text-center">
-          Give the gift of a clean home to someone with a busy lifestyle
-        </h4>
-        <h4 className="text-center"> More clean, less stress</h4>
-
-        <div className="row">
-          <div className="col-lg-4" />
-          <div className="col-lg-4">
-            <div className="text-center">
-              <div className="form-group">
-                <label htmlFor="amount">
-                  <h5>Amount:</h5>
-                </label>
-                <input
-                  placeholder="Dollar Amount"
-                  type="text"
-                  className="form-control"
-                  id="amount"
-                  name="amount"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4" />
+        <div>
+          <h4 className="busy text-center">
+            Give the gift of a clean home to someone with a busy lifestyle.
+          </h4>
+          <h4 className="text-center"> More clean, less stress!</h4>
         </div>
 
         <div className="row">
+          <div className="col-lg-4">
+            <div className="text-center">
+              <img
+                className="discount"
+                id="discount"
+                src={discount}
+                height={300}
+                width={400}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="col-lg-4" />
+          <div className="col-lg-4">
+            <div className="text-center pb-3">
+              <img
+                className="gift"
+                id="gift"
+                src={gift}
+                height={300}
+                width={600}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="row pt-1">
           <div className="col-lg-6">
-            <h5>From:</h5>
+            <h4>From:</h4>
             <section id="from">
               <div align="left">
                 <form>
@@ -52,6 +61,7 @@ export default class GiftCertificates extends Component {
                       name="name"
                     />
                   </div>
+
                   <div className="form-group">
                     <label htmlFor="street">Street Address</label>
                     <input
@@ -62,6 +72,7 @@ export default class GiftCertificates extends Component {
                       name="street"
                     />
                   </div>
+
                   <div className="form-group">
                     <label htmlFor="csz">City, State, Zip</label>
                     <input
@@ -75,59 +86,69 @@ export default class GiftCertificates extends Component {
                 </form>
               </div>
             </section>
-            <div className="text-center">
-              <img
-                className="discount"
-                id="discount"
-                src={discount}
-                height={400}
-                width={500}
-                alt=""
+          </div>
+
+          <div className="col-lg-6">
+            <h4>To:</h4>
+
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                placeholder="Name"
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="street">Street Address</label>
+              <input
+                placeholder="Street Address"
+                type="text"
+                className="form-control"
+                id="street"
+                name="street"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="csz">City, State, Zip</label>
+              <input
+                placeholder="City, State, Zip"
+                type="text"
+                className="form-control"
+                id="city"
+                name="city"
               />
             </div>
           </div>
-          <div className="col-lg-6">
-            <section id="to">
-              <div>
-                <h5>To:</h5>
-                <form>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      placeholder="Name"
-                      type="text"
-                      className="form-control"
-                      id="name"
-                      name="name"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="street">Street Address</label>
-                    <input
-                      placeholder="Street Address"
-                      type="text"
-                      className="form-control"
-                      id="street"
-                      name="street"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="csz">City, State, Zip</label>
-                    <input
-                      placeholder="City, State, Zip"
-                      type="text"
-                      className="form-control"
-                      id="city"
-                      name="city"
-                    />
-                  </div>
-                </form>
-              </div>
-            </section>
-            <div className="text-center">
-              <img className="gift" id="gift" src={gift} alt="" />
+        </div>
+
+        <div className="row pt-1">
+          <div className="col-lg-4" />
+
+          <div className="col-lg-4">
+            <div className="text-centerform-group">
+              <label htmlFor="amount">
+                <h5>Amount:</h5>
+              </label>
+              <input
+                placeholder="Dollar Amount"
+                type="text"
+                className="form-control"
+                id="amount"
+                name="amount"
+              />
             </div>
           </div>
+
+          <div className="col-lg-4" />
+        </div>
+
+        <div className="gcb btn btn-block mb-4">
+          <Link>Submit</Link>
         </div>
       </div>
     );
