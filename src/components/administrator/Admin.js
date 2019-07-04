8639b71d1d7e5b7be92c8c01542177c24cbba5c2
publaptops <jamespulley59@gmail.com>
2019-07-03 22:02:13 -0400

    sorry lillian, i started working in master

--- src/components/administrator/Admin.js
+++ src/components/administrator/Admin.js
@@ -4,7 +4,20 @@ export default class Admin extends Component {
     render() {
         return (
             <div>
-                
+                <div className='row'>
+                    <div classname='col-lg-3'>
+                        <h4>Client Name/Id? Including address, etc...</h4>
+                    </div>
+                    <div classname='col-lg-2'>
+                        <h2>Date of last svs/estimate</h2>
+                    </div>
+                    <div classname='col-lg-7'>
+                        <h2>Last svs</h2>
+                    </div>
+                    <div classname='col-lg-1'>
+                        <h2>Charge</h2>
+                    </div>
+                </div>
             </div>
         )
     }
