e5f529e1e3c25116852d4e9ec4c7d9016bbfae4f
jamespulley59 <jamespulley59@gmail.com>
2019-07-10 14:06:26 -0400

    fixes

--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -5,12 +5,13 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark">
-        <Link to="/Login">Login</Link>
+        <Link to="/Login">Login/Register</Link>
         <Link to="/ScheduleServices">Schedule Service(s)</Link>
         <Link to="/BeforeAndAfter">Before and After Photos</Link>
         <Link to="/GiftCertificates">Gift Certificates Available</Link>
         <Link to="/Comments">Comments?</Link>
         <Link to="home">Home Page</Link>
+        <Link to="profile">Profile</Link>
       </nav>
     );
   }
